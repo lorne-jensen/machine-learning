5e5e4c7739a01e7aada86f6f9ce81589b93efe8f
lorne-jensen <116099007+lorne-jensen@users.noreply.github.com>
2022-11-05 16:03:36 -0600

    trying to get something working together

--- lstm_chatbot/src/model.py
+++ lstm_chatbot/src/model.py
@@ -1,14 +1,29 @@
+import torch
+from numpy import random
 from torch import nn
 
 
 class Encoder(nn.Module):
 
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, embedding_size):
         super(Encoder, self).__init__()
 
         # self.embedding provides a vector representation of the inputs to our model
 
         # self.lstm, accepts the vectorized input and passes a hidden state
+
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
+
+        self.hidden = torch.zeros(1, 1, hidden_size)
+
+        self.embedding = nn.Embedding(num_embeddings=self.input_size,
+                                      embedding_dim=self.embedding_size)
+        # Initialize LSTM; the input_size and hidden_size params are both set to 'hidden_size'
+        #   because our input size is a word embedding with number of features == hidden_size
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
 
     def forward(self, i):
         '''
@@ -17,35 +32,72 @@ class Encoder(nn.Module):
                 h, the hidden state
                 c, the cell state
         '''
-
-        return o, h, c
+        # Convert word indexes to embeddings
+        embedded = self.embedding(i)
+        # Pack padded batch of sequences for RNN module
+        # Forward pass through LSTM
+        return self.lstm(embedded)
 
 
 class Decoder(nn.Module):
 
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, hidden_size, output_size, embedding_size):
         super(Decoder, self).__init__()
 
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.embedding_size = embedding_size
+
         # self.embedding provides a vector representation of the target to our model
+        self.embedding = nn.Embedding(num_embeddings=self.output_size,
+                                      embedding_dim=self.embedding_size)
 
         # self.lstm, accepts the embeddings and outputs a hidden state
+        self.lstm = nn.LSTM(self.embedding_size, hidden_size, num_layers=3)
 
         # self.ouput, predicts on the hidden state via a linear output layer
+        self.output = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, i, h):
-        '''
+    def forward(self, i, h, c):
+        """
         Inputs: i, the target vector
         Outputs: o, the prediction
                 h, the hidden state
-        '''
+        """
+        v = i.unsqueeze(0)
 
-        return o, h
+        v = self.embedding(v)
+
+        o, h, c = self.lstm(v, (h, c))
+
+        p = self.output(o.squeeze(0))
+
+        return o, h, p, c
 
 
 class Seq2Seq(nn.Module):
 
-    def __init__(self, encoder_input_size, encoder_hidden_size, decoder_hidden_size, decoder_output_size):
+    def __init__(self, encoder_input_size, encoder_hidden_size, encoder_embedding_size,
+                 decoder_hidden_size, decoder_output_size, decoder_embedding_size):
         super(Seq2Seq, self).__init__()
+        self.encoder = Encoder(encoder_input_size, encoder_hidden_size, encoder_embedding_size)
+        self.decoder = Decoder(decoder_hidden_size, decoder_output_size, decoder_embedding_size)
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_size
+
+        o = torch.zeros(trg_len, batch_size, trg_vocab_size)
+
+        h, c = self.encoder(src)
+
+        x = trg[0, :]
+        for t in range(1, trg_len):
+            o, h, c = self.decoder(x, h, c)
+            o[t] = o
+            teacher_force = random.random() < teacher_forcing_ratio
+            top_trg = o.argmax(1)
+            x = trg[t] if teacher_force else top_trg
+
         return o
--- lstm_chatbot/src/prepare_data.py
+++ lstm_chatbot/src/prepare_data.py
@@ -23,10 +23,19 @@ def loadDF(path):
     You will use this function to load the dataset into a Pandas Dataframe for processing.
     """
     # torchtext.datasets.SQuAD1(root: str = '.data', split: Union[Tuple[str], str] = ('train', 'dev'))
-    df = torchtext.datasets.SQuAD1(path, ('train', 'test'))
+    train_iter, dev_iter = torchtext.datasets.SQuAD1(path)  #, split=('train', 'test'))
 
-    df.set_format(type='pandas')
-    return df
+    df = {
+        "question": [],
+        "answer": []
+    }
+
+    for context, question, answers, indices in train_iter:
+        if answers[0]:
+            df["question"].append(question)
+            df["answer"].append(answers[0])
+
+    return pd.DataFrame.from_dict(df)
 
 
 def prepare_text(sentence):
@@ -35,17 +44,30 @@ def prepare_text(sentence):
     https://www.nltk.org/api/nltk.tokenize.html
     """
     tokens = nltk.tokenize.sent_tokenize(sentence)
-    tokens = [nltk.tokenize.word_tokenize(t) for t in tokens]  # TODO not sure yet...
+    # tokens = [nltk.tokenize.word_tokenize(t) for t in tokens]  # TODO not sure yet...
     return tokens
 
 
 def train_test_split(SRC, TRG):
-    '''
-    Input: SRC, our list of questions from the dataset
+    """
+    Input: SRC, our list of questions from the dataset, should be a tuple of (train_df, test_df)
             TRG, our list of responses from the dataset
 
     Output: Training and test datasets for SRC & TRG
 
-    '''
-
+    """
+    SRC_train_dataset = SRC[0]
+    SRC_test_dataset = SRC[1]
+    TRG_train_dataset = TRG[0]
+    TRG_test_dataset = TRG[1]
+    # SRC_train_dataset = train_df["question"].tolist()
+    # TRG_train_dataset = train_df["answer"].tolist()
+    #
+    # SRC_test_dataset = test_df["question"].tolist()
+    # TRG_test_dataset = test_df["answer"].tolist()
     return SRC_train_dataset, SRC_test_dataset, TRG_train_dataset, TRG_test_dataset
+
+
+if __name__ == '__main__':
+    f = loadDF('./')
+    print('yo')
